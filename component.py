6b36190d53597fc6b0701b46ace2ce90d7c25d16
Mati <mateuszlabun9@gmail.com>
2022-03-07 01:15:35 +0100

    warmup tasks

--- component.py
+++ component.py
@@ -1,12 +1,5 @@
 class A:
-    def __init__(self):
-        pass
-    def __len__(self):
-        pass
-    def __iter__(self):
-        pass
-    def __getitem__(self, item):
-        pass
+    pass
 
 
 class B:
